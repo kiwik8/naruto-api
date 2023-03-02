f89609bd9563f3e40758b94831d154c7aa86cf92
Martin Gouverneur <gouverneur.martin@hotmail.com>
2023-03-02 18:30:26 +0100

    +

--- naruto/settings.py
+++ naruto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)d=&0p07p(9_y)ib=_)dt-=&_vej(#5k1)jvd(h(2k3ij^&20#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
